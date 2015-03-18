356bfd0468f5041f03cc2c21a343560ac12a132a
Florent33 <ferrand.florent@gmail.com>
2015-03-18 10:57:10 +0100

    initial commit

---
+++ APIJeuMorpionCsharp/APIJeuMorpionCsharp/Classes/Coordonnee.cs
@@ -0,0 +1,47 @@
+﻿using APIJeuMorpionCsharp.Globales;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+/**
+ * Coordonnée du pion X Y
+ **/
+namespace APIJeuMorpionCsharp.Classes
+{
+    public class Coordonnee : ICloneable
+    {
+        private string _id;
+
+        public Coordonnee(int x, int y)
+        {
+            X = x;
+            Y = y;
+            _id = MethodesGlobales.ConvertirCoordonneEnIdCase(x, y);
+        }
+
+        public Coordonnee(string idCase)
+        {
+            this._id = idCase;
+            ExtraireCoordonnees(idCase);
+        }
+
+        private void ExtraireCoordonnees(string idCase)
+        {
+            X = MethodesGlobales.ConvertirIdCaseEnCoordonnee(idCase).X;
+            Y = MethodesGlobales.ConvertirIdCaseEnCoordonnee(idCase).Y;
+        }
+        /// <summary>
+        ///  
+        /// </summary>
+        public int X{ get; set; }
+        public int Y { get; set; }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
